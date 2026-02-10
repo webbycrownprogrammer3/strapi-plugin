70872c7e5f82944da3ba737d23fc3c08f84003ad
programmer3 <programmer3@webbycrown.com>
2026-02-10 13:50:07 +0530

    Export admin plugin default for Strapi v5

--- strapi-admin.js
+++ strapi-admin.js
@@ -4,5 +4,9 @@
  * Strapi plugin admin entrypoint.
  * Strapi loads this file to register the plugin in the Admin panel.
  */
-module.exports = require('./dist/admin');
+// For webpack bundle that sets `module.exports.default = plugin`
+// we need to export the actual default export so Strapi gets the plugin object,
+// not the wrapper module.
+const admin = require('./dist/admin');
+module.exports = admin.default || admin;
 
