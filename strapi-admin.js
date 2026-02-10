bf4ebbf6b1ea9dccc398762ed4dee61084a9318e
programmer3 <programmer3@webbycrown.com>
2026-02-10 13:15:45 +0530

    Add Strapi entrypoints for admin/server

---
+++ strapi-admin.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/**
+ * Strapi plugin admin entrypoint.
+ * Strapi loads this file to register the plugin in the Admin panel.
+ */
+module.exports = require('./dist/admin');
+
---
+++ strapi-server.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/**
+ * Strapi plugin server entrypoint.
+ * Strapi loads this file to register the plugin on the server side.
+ */
+module.exports = require('./dist/server');
+
